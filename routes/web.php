7d969a2cc63e71120e0aed823ec8df268dc568f0
Dani-rizqullah <danirizqullahpro@gmail.com>
2023-12-25 03:09:48 +0700

    bentuk route baru

--- routes/web.php
+++ routes/web.php
@@ -24,38 +24,78 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::middleware(['guest'])->group(function(){ // yang belum login
-    Route::get('/', [SesiController::class, 'index'])->name('login'); //untuk login
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', [SesiController::class, 'index'])->name('login'); // Login
     Route::post('/', [SesiController::class, 'login']);
-    
 });
-Route::get('/home', function(){
-    return view('admin.dashboard.index'); 
 
+// Routes for authenticated users
+Route::middleware(['auth'])->group(function () {
+    Route::get('/logout', [SesiController::class, 'logout']); // Logout
+
+    // Admin routes with 'admin' prefix and middleware
+    Route::middleware('userAkses:admin')->prefix('admin')->group(function () {
+        Route::resource('/dapil', DapilController::class, ['as'=>'admin']);
+        Route::resource('/kecamatan', KecamatanController::class, ['as'=>'admin']);
+        Route::resource('/kelurahan', KelurahanController::class, ['as'=>'admin']);
+        Route::resource('/tpsuara', TpsuaraController::class, ['as'=>'admin']);
+        Route::resource('/partai', PartaiController::class, ['as'=>'admin']);
+        Route::resource('/caleg', CalegController::class, ['as'=>'admin']);
+        Route::resource('/rekap_suara_caleg', RscController::class, ['as'=>'admin']);
+        Route::resource('/rekap_suara_partai', RekapsuarapartaiController::class, ['as'=>'admin']);
+        Route::resource('/laporan', LaporanController::class, ['as'=>'admin']);
+    });
+
+    // Saksi routes with 'saksi' prefix and middleware
+    Route::middleware('userAkses:saksi')->prefix('saksi')->group(function () {
+        Route::resource('/rekap_suara_caleg', RscController::class, ['as'=>'admin']);
+        Route::resource('/rekap_suara_partai', RekapsuarapartaiController::class, ['as'=>'admin']);
+    });
+
+    // Home route for all authenticated users
+    Route::get('/home', function () {
+        return view('admin.dashboard.index');
+    });
 });
-Route::middleware(['auth'])->group(function(){ // untuk bagian yang telah login
+
+
+
+
+
+
+// Route::middleware(['guest'])->group(function(){ // yang belum login
+//     Route::get('/', [SesiController::class, 'index'])->name('login'); //untuk login
+//     Route::post('/', [SesiController::class, 'login']);
     
-    Route::get('/logout', [SesiController::class, 'logout']); //untuk log out
+// });
+// Route::get('/home', function(){
+//     return view('admin.dashboard.index'); 
 
-    Route::resource('/dapil', DapilController::class, ['as'=>'admin'])->middleware('userAkses:admin');
-
-    Route::resource('/kecamatan', KecamatanController::class, ['as'=>'admin'])->middleware('userAkses:admin');
-
-    Route::resource('/kelurahan', KelurahanController::class, ['as'=>'admin'])->middleware('userAkses:admin');
-
-    Route::resource('/tpsuara', TpsuaraController::class, ['as'=>'admin'])->middleware('userAkses:admin');
-
-    Route::resource('/partai', PartaiController::class, ['as'=>'admin'])->middleware('userAkses:admin');
-
-    Route::resource('/caleg', CalegController::class, ['as'=>'admin'])->middleware('userAkses:admin');
-
-    Route::resource('/rekap_suara_caleg', RscController::class, ['as'=>'admin'])->middleware('userAkses:saksi');
-
-    Route::resource('/rekap_suara_partai', RekapsuarapartaiController::class, ['as'=>'admin'])->middleware('userAkses:saksi');
-
-    Route::resource('/laporan', LaporanController::class, ['as'=>'admin']);
+// });
+// Route::middleware(['auth'])->group(function(){ // untuk bagian yang telah login
     
-});
+//     Route::get('/logout', [SesiController::class, 'logout']); //untuk log out
+
+//     Route::resource('/dapil', DapilController::class, ['as'=>'admin'])->middleware('userAkses:admin');
+
+//     Route::resource('/kecamatan', KecamatanController::class, ['as'=>'admin'])->middleware('userAkses:admin');
+
+//     Route::resource('/kelurahan', KelurahanController::class, ['as'=>'admin'])->middleware('userAkses:admin');
+
+//     Route::resource('/tpsuara', TpsuaraController::class, ['as'=>'admin'])->middleware('userAkses:admin');
+
+//     Route::resource('/partai', PartaiController::class, ['as'=>'admin'])->middleware('userAkses:admin');
+
+//     Route::resource('/caleg', CalegController::class, ['as'=>'admin'])->middleware('userAkses:admin');
+
+//     Route::resource('/rekap_suara_caleg', RscController::class, ['as'=>'admin'])->middleware('userAkses:saksi');
+
+//     Route::resource('/rekap_suara_partai', RekapsuarapartaiController::class, ['as'=>'admin'])->middleware('userAkses:saksi');
+
+    // Route::resource('/laporan', LaporanController::class, ['as'=>'admin']);
+    
+// });
 
 
 
